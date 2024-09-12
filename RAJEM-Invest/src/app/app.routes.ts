c04e852dbbe3b72cd87b65820bed0b45fea23ae3
Rodrigo Flores <dii_goo@hotmail.com>
2024-09-11 21:42:55 -0300

    Commit Incial

---
+++ RAJEM-Invest/src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+
+export const routes: Routes = [
+  { path: '', component: LoginComponent }, // Define LoginComponent como a rota padrão
+  // Outras rotas podem ser adicionadas aqui
+];
+

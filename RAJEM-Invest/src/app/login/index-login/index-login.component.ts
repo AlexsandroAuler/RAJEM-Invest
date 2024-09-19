4adaecc2ef8e866b1c89de9622b3c66bfbdb9604
Rodrigo Flores <dii_goo@hotmail.com>
2024-09-19 02:46:27 -0300

    Foi ajustado o comportamento e a relação inicial entre os componentes, foi criado algumas rotas para o backend.

---
+++ RAJEM-Invest/src/app/login/index-login/index-login.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-index-login',
+  standalone: true,  // Habilitando standalone
+  imports : [FormsModule, CommonModule],
+  templateUrl: './index-login.component.html',
+  styleUrls: ['./index-login.component.css']
+})
+export class IndexLoginComponent {
+  username: string = '';
+  password: string = '';
+
+  constructor(private router: Router) {}
+
+  // Função para navegar até "primeiro-login"
+  goToPrimeiroLogin() {
+    this.router.navigate(['/primeiro-login']);
+  }
+  gotoLogin() {
+    // Lógica para fazer login (pode incluir autenticação)
+    console.log('Usuário:', this.username);
+    console.log('Senha:', this.password);
+
+    if (this.username === 'rodrigo' && this.password.trim() === ''){
+      this.router.navigate(['/liberar-acesso']);
+    }else{
+      alert('Informe Login e senha')
+    }
+  }
+}
+
+

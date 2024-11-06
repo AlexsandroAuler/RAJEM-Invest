6465f89cf965c45958e1a3e003969c0c3b8e1dcc
Rodrigo Flores <dii_goo@hotmail.com>
2024-11-05 21:07:46 -0300

    Criacao CRUD carteiras - Inicial

---
+++ RAJEM-Invest/src/app/carteiras/carteira/criar-carteira/criar-carteira.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-criar-carteira',
+  standalone: true,
+  imports: [],
+  templateUrl: './criar-carteira.component.html',
+  styleUrl: './criar-carteira.component.css'
+})
+export class CriarCarteiraComponent {
+
+}
---
+++ RAJEM-Invest/src/app/carteiras/carteira/nova-carteira/nova-carteira.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-nova-carteira',
+  standalone: true,
+  imports: [],
+  templateUrl: './nova-carteira.component.html',
+  styleUrl: './nova-carteira.component.css'
+})
+export class NovaCarteiraComponent {
+
+}

5c503f8d9b7c52e5f5f51408470859d10314017e
Rodrigo Flores <dii_goo@hotmail.com>
2024-11-08 02:06:58 -0300

    List_Carteiras

--- RAJEM-Invest/src/app/carteiras/carteira/list-carteiras/list-carteiras.component.ts
+++ RAJEM-Invest/src/app/carteiras/carteira/list-carteiras/list-carteiras.component.ts
@@ -1,21 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormsModule } from '@angular/forms'; // Importa FormsModule
-import { CommonModule } from '@angular/common'; // Importando CommonModule
 import { AuthService } from '../../../services/auth.service';
+import { firstValueFrom} from 'rxjs';
+
+
 @Component({
   selector: 'app-list-carteiras',
   standalone: true,
   imports: [],
   templateUrl: './list-carteiras.component.html',
-  styleUrl: './list-carteiras.component.css'
+  styleUrls: ['./list-carteiras.component.css']
 })
-export class ListCarteirasComponent {
-  nomeCarteira : string = '';
+export class ListCarteirasComponent implements OnInit {
+  nomeCarteira: string = '';
+  carteiras: any[] = [];
+  carteiraSelecionada: any = null;
 
-  constructor(private authService: AuthService, private router: Router){}
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
-  criarCarteira(){
-      this.router.navigate(['/criar-carteira']);
+  async ngOnInit(): Promise<void> {
+    await this.carregarCarteiras();
+  }
+
+  async carregarCarteiras(): Promise<void> {
+    try {
+      this.carteiras = await firstValueFrom(this.authService.getCarteiras());
+    } catch (erro) {
+      console.error('Erro ao carregar carteiras:', erro);
+    }
+  }
+  
+
+  criarCarteira(): void {
+    this.router.navigate(['/criar-carteira']);
+  }
+
+  selecionarCarteira(carteira: any): void {
+    this.carteiraSelecionada = carteira;
   }
 }
+
+
+
+
